c59598febf3e29ed33dd713da501be3ac28bc0a3
RaglandDev <alexragland2003@gmail.com>
2024-03-28 15:58:09 -0700

    camera movement inside car

--- scripts/car.gd
+++ scripts/car.gd
@@ -4,16 +4,27 @@ var max_rpm = 500
 var max_torque = 200
 var active = false
 var car_zone = false
+var hood_zone = false
+var door_open = false
+var inside_car = true
+
+@onready var neck := $Neck_Car
+@onready var camera := $Neck_Car/camera_car
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+func _unhandled_input(event):
+		if event is InputEventMouseMotion && inside_car:
+			neck.rotate_y(-event.relative.x * .01)
+			camera.rotate_x(-event.relative.y * .01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if active:
-		$camera_car.make_current()
+		$Neck_Car/camera_car.make_current()
 		
 		steering = lerp(steering, Input.get_axis("right", "left") * 0.4, 5 * delta)
 		engine_force = Input.get_axis("back", "forward") 
@@ -28,6 +39,7 @@ func _process(delta):
 		leaving_car()
 	elif !active:
 		entering_car()
+		opening_hood()
 
 
 func _on_player_detect_body_entered(body):
@@ -37,16 +49,34 @@ func _on_player_detect_body_exited(body):
 	if body.name == "player":
 		car_zone = false
 func entering_car():
-	if Input.is_action_just_pressed("ui_e") && car_zone:
+	if Input.is_action_just_pressed("ui_e") && car_zone && !door_open:
+		get_parent().find_child("AnimationPlayer").play("Door_L Open")
+		door_open = true
+	elif Input.is_action_just_pressed("ui_e") && car_zone && door_open:
+		get_parent().find_child("AnimationPlayer").play_backwards("Door_L Open")
 		var hidden_player = get_parent().get_node("player")
 		hidden_player.active = false
-		$camera_car.make_current()
+		$Neck_Car/camera_car.make_current()
 		active = true
+		door_open = false
+		inside_car = true
+
+		
 func leaving_car():
 	var vehicle = $"."
 	var hidden_player = get_parent().get_node("player")
-	var newLoc = vehicle.global_transform.origin - 2 * vehicle.global_transform.basis.x
+	var newLoc = vehicle.global_transform.origin - .5 * vehicle.global_transform.basis.x
 	if Input.is_action_just_pressed("ui_e"):
 		hidden_player.active = true
 		active = false 
+		inside_car = false
 		hidden_player.global_transform.origin = newLoc
+
+
+func _on_player_detect_hood_body_entered(body):
+	if body.name == "player":
+		hood_zone = true
+func opening_hood():
+	#if Input.is_action_just_pressed("ui_e") && hood_zone:
+		#get_parent().find_child("AnimationPlayer").play("Door_L Open")
+	pass
